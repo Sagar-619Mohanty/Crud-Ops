2365f23e9990bc5327dec40d2ad5b58a0bb50ed3
Sagar-619Mohanty <143720093+Sagar-619Mohanty@users.noreply.github.com>
2023-09-09 20:59:08 +0530

    Add files via upload

---
+++ SubjectController.java
@@ -0,0 +1,35 @@
+package com.xadmin.SpringBootCrud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xadmin.SpringBootCrud.bean.Subject;
+import com.xadmin.SpringBootCrud.service.SubjectService;
+@RestController
+public class SubjectController {
+	@Autowired
+    private SubjectService subjectService;
+    @RequestMapping("/subjects")
+    public List<Subject> getAllsubjects(){
+      return subjectService.getAllSubjects();
+    }
+    @RequestMapping(method = RequestMethod.POST , value="/subjects")
+    public void addSubject(@RequestBody Subject subject){
+        subjectService.addSubject(subject);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT , value="/subjects/{id}")
+    public void updateSubject( @PathVariable int id , @RequestBody Subject subject){
+        subjectService.updateSubject(id, subject);
+    }
+    @RequestMapping(method = RequestMethod.DELETE , value="/subjects/{id}")
+    public void DeleteSubject(@PathVariable int id ){
+        subjectService.deleteSubject(id);
+    }
+}
